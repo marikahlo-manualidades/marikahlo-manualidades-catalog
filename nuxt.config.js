8ff3473a96d7d45e3d8a3006d002c1c7bccb4e65
gabrielf-andrade <gabrielf_andrade@hotmail.com>
2022-09-20 23:07:20 -0300

    changed head description

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Artesanatos variados em feltro - envio para todo o Brasil' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
