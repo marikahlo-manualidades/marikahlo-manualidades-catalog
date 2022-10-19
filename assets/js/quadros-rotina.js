d4c0dec33a4cdff2b561c81f14819170780847df
gabrielf-andrade <gabrielf_andrade@hotmail.com>
2022-10-19 18:52:45 -0300

    QuadrosRotina photos changed and added

--- assets/js/quadros-rotina.js
+++ assets/js/quadros-rotina.js
@@ -1,85 +1,102 @@
 export const quadrosRotina = {
-    valor: "Valor: R$200,00",
-    descricao: [
-      "Contém os 7 dias da semana, 4 tempos do dia e 21 plaquinhas de atividades (que serão confeccionadas conforme escolha do cliente).",
-      "Acima desse número, terá custo adicional por plaquinha",
-      "Vem com um saquinho para guardar o material",
-      "Dimensões: 30cm de altura x 50cm de largura",
-      "Prazo de entrega de no mínimo 30 dias."
-    ],
-    imagens: [
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina01.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina02.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina03.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina04.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina05.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina06.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina07.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina08.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina09.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina10.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina11.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina12.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina13.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina14.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina15.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina16.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina17.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina18.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina19.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina20.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina21.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina22.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina23.jpg")
-      },
-      {
-        imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina24.jpg")
-      }
-    ]
-  };
-  
+  valor: "Valor: R$200,00",
+  descricao: [
+    "Contém os 7 dias da semana, 4 tempos do dia e 21 plaquinhas de atividades (que serão confeccionadas conforme escolha do cliente).",
+    "Acima desse número, terá custo adicional por plaquinha",
+    "Vem com um saquinho para guardar o material",
+    "Dimensões: 30cm de altura x 50cm de largura",
+    "Prazo de entrega de no mínimo 30 dias."
+  ],
+  imagens: [
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina01.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina02.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina03.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina04.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina05.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina06.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina07.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina08.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina09.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina10.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina11.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina12.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina13.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina14.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina15.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina16.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina17.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina18.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina19.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina20.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina21.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina22.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina23.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina24.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina25.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina26.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina27.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina28.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina29.jpg")
+    },
+    {
+      imagem: require("~/assets/image/produtos/quadros-rotina/quadro-rotina30.jpg")
+    }
+  ]
+};
